a9108bc11b3b74f82e7d9f73f8874218ae998315
Herkahahaha <herkahahaha@gmail.com>
2019-10-10 21:39:45 +0700

    add play hooks file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Game from "./components/Game";
+import PlayHook from "./components/hooks/PlayHook";
 
 function App() {
   return (
     <div className="App" style={{ textAlign: "center" }}>
-      <h1>Hello React Tutorial</h1>
+      <h1>Class Component</h1>
       <Game />
+      <hr />
+      <h1>Hooks</h1>
+      <PlayHook />
     </div>
   );
 }
